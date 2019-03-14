376da70203d76bf82b328781cac133a0c95138c6
bhargavbunny <bhargavreddy1994u@gmail.com>
2019-03-14 09:37:50 -0400

    new changes made to the home page with nav-bar

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="row">
+    <div class="col1">
+    <h1>This is {{name}}'s page. He's {{age}} years old</h1>
+    <input v-model="age" type="text" class = "force-control" placeholder="age" />
+<div class="progress">
+  <div class="progress-bar" role="progressbar" :style="{width: '75%'}" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data: ()=> ({
+    name :"bhargav reddy",
+    age :"25"
+  }) 
+}
+</script>
+
+<style type="scss">
+
+</style>
